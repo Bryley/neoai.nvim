33723e180c34df20ed4adec49abf33a08f3f2d36
Jongwook Choi <wookayin@gmail.com>
2025-07-24 20:22:13 -0400

    fix: Handle error when OPENAI_API_KEY is not set. (#24)
    Co-authored-by: Bryley <bryleyhayter@gmail.com>

--- lua/neoai/chat/models/openai.lua
+++ lua/neoai/chat/models/openai.lua
@@ -51,32 +51,32 @@ end
 ---@param chat_history ChatHistory
 ---@param on_stdout_chunk fun(chunk: string) Function to call whenever a stdout chunk occurs
 ---@param on_complete fun(err?: string, output?: string) Function to call when model has finished
-M.send_to_model = function(chat_history, on_stdout_chunk, on_complete)
-    local api_key = config.options.open_ai.api_key.get()
+M.send_to_model = function (chat_history, on_stdout_chunk, on_complete)
+    local api_key = os.getenv(config.options.open_api_key_env)
+    if not api_key then
+      on_complete("OpenAI API Key is missing.", nil)
+      return
+    end
 
     local data = {
         model = chat_history.model,
-        stream = true,
-        messages = chat_history.messages,
+      	stream = true,
+        messages = chat_history.messages
     }
-    data = vim.tbl_deep_extend("force", {}, data, chat_history.params)
-
     chunks = {}
     raw_chunks = {}
     utils.exec("curl", {
-        "--silent",
-        "--show-error",
-        "--no-buffer",
-        "https://api.openai.com/v1/chat/completions",
+        "--silent", "--show-error", "--no-buffer", "https://api.openai.com/v1/chat/completions",
         "-H",
         "Content-Type: application/json",
         "-H",
         "Authorization: Bearer " .. api_key,
         "-d",
         vim.json.encode(data),
-    }, function(chunk)
+    }, function (chunk)
         M._recieve_chunk(chunk, on_stdout_chunk)
-    end, function(err, _)
+    end, function (err, _)
+
         local total_message = table.concat(raw_chunks, "")
         local ok, json = pcall(vim.json.decode, total_message)
         if ok then
